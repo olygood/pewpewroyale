0a964e3f80c0afe1e460860d8864646610cdec72
Technifutur <neildidi33@gmail.com>
2018-01-09 15:01:45 +0100

    PlayerSelection reset at awake + (incomplete) playerSelection

---
+++ Assets/_pewpewroyale/Scripts/ScriptableObjects/PlayerData.cs
@@ -0,0 +1,8 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu]
+public class PlayerData : ScriptableObject {
+
+    public int characterType;
+}
---
+++ Assets/_pewpewroyale/Scripts/SelectCharacter.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SelectCharacter : MonoBehaviour {
+
+    public GameData currentGame;
+
+    public int m_playerId;
+	
+	public void Pick(int _selection)
+    {
+        Debug.Log(m_playerId + " " + _selection);
+    }
+}
