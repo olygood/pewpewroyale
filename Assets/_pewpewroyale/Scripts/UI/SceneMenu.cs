aa3bbb1bc68ec2d8a10dd770246e350c5a041c2d
Technifutur <neildidi33@gmail.com>
2018-01-09 12:16:26 +0100

    SceneMenu beginning

---
+++ Assets/_pewpewroyale/Scripts/UI/SceneMenu.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneMenu : MonoBehaviour
+{
+    public void NewGame()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    }
+
+    public void ExitGame()
+    {
+        Application.Quit();
+    }
+
+    public void Options()
+    {
+
+    }
+
+    public void Credit()
+    {
+
+    }
+	
+}
