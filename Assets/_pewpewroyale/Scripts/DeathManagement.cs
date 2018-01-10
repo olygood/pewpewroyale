4c58efd7f01cdb35cc09ec945dcdb0d0255f03e3
Unknown <neildidi33@gmail.com>
2018-01-10 12:34:11 +0100

    Respawn()

--- Assets/_pewpewroyale/Scripts/DeathManagement.cs
+++ Assets/_pewpewroyale/Scripts/DeathManagement.cs
@@ -6,21 +6,22 @@ public class DeathManagement : MonoBehaviour {
 
     private GameObject player;
     public LevelData m_levelData;
-    public Move m_move;
+    public InputManager m_inputManager = new InputManager();
 
-    private void Awake()
-    {
-        m_move = new Move();
-    }
-
+  
     void OnCollisionEnter2D(Collision2D coll)
     {
         if (coll.gameObject.tag == "bullet")
         {
-            int id = m_move.m_playerId;
+            Respawn();
+        }
+    }
+
+    private void Respawn()
+    {
+            int id = m_inputManager.m_playerId;
             Vector2 vect = m_levelData.spawnPoints[id];
             transform.position = vect;
-        }
     }
 }
 
