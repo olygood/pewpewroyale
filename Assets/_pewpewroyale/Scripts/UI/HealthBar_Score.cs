c856651f4f7b175a25f16634eae32526e8f867f2
Unknown <neildidi33@gmail.com>
2018-01-10 16:00:37 +0100

    Auto stash before merge of "master" and "origin/master"

---
+++ Assets/_pewpewroyale/Scripts/UI/HealthBar_Score.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class HealthBar_Score : MonoBehaviour
+{
+    public int health = 100;
+    public Image healthBar;
+
+    private GameObject player;
+    public LevelData m_levelData;
+    public InputManager m_inputManager = new InputManager();
+
+  
+
+    void OnCollisionEnter2D(Collision2D coll)
+    {
+        if (coll.gameObject.tag == "bullet")
+        {
+            TakeDamage(10);
+        }
+    }
+
+    public void TakeDamage(int amount)
+    {
+        health -= amount;
+
+        healthBar.fillAmount = health;
+
+        if (health <= 0)
+        {
+            Respawn();
+        }
+    }
+  
+    public void Respawn()
+    {
+        int id = m_inputManager.m_playerId;
+        Vector2 vect = m_levelData.spawnPoints[id];
+        transform.position = vect;
+    }
+}
